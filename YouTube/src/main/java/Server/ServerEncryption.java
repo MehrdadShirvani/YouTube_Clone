4aa31fbb3dbaa475c537194f9cea3f48cf47c328
woxane <mohsen.minavand1384@gmail.com>
2024-06-06 19:51:38 +0330

    Implement generateKeyPair functoin in ServerEncryption class

--- YouTube/src/main/java/Server/ServerEncryption.java
+++ YouTube/src/main/java/Server/ServerEncryption.java
@@ -1,2 +1,12 @@
-package Server;public class ServerEncryption {
+package Server;
+
+
+import java.security.*;
+
+public class ServerEncryption {
+    public static KeyPair generateKeyPair() throws Exception {
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+        keyPairGenerator.initialize(2048);
+        return keyPairGenerator.generateKeyPair();
+    }
 }
