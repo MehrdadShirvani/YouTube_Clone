5533e20e143ec9f38c6d790671783ed5c3cf40a1
woxane <mohsen.minavand1384@gmail.com>
2024-06-06 19:58:00 +0330

    Add constructor to ServerEncryption class and add try catch to generateKeyPair function

--- YouTube/src/main/java/Server/ServerEncryption.java
+++ YouTube/src/main/java/Server/ServerEncryption.java
@@ -4,9 +4,25 @@ package Server;
 import java.security.*;
 
 public class ServerEncryption {
-    public static KeyPair generateKeyPair() throws Exception {
-        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
-        keyPairGenerator.initialize(2048);
-        return keyPairGenerator.generateKeyPair();
+    private final PublicKey serverPublicKey;
+    private final PrivateKey serverPrivateKey;
+
+    public ServerEncryption() {
+        KeyPair keyPair =  generateKeyPair() ;
+        this.serverPublicKey = keyPair.getPublic();
+        this.serverPrivateKey = keyPair.getPrivate();
+    }
+
+    public KeyPair generateKeyPair() {
+        try {
+            KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA");
+            keyPairGenerator.initialize(2048);
+            return keyPairGenerator.generateKeyPair();
+        } catch (Exception e) {
+            System.err.println("ERROR : while generating KeyPair inside the server ! : ");
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
