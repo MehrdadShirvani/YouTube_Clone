d2f71ca8c5e0d856b0f60dfd9a536e9095c2a56b
woxane <mohsen.minavand1384@gmail.com>
2024-06-08 17:09:05 +0330

    implement `writeLog` function in ClientHandler class

--- YouTube/src/main/java/Server/ClientHandler.java
+++ YouTube/src/main/java/Server/ClientHandler.java
@@ -4,10 +4,13 @@ import Client.ClientEncryption;
 
 import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ClientHandler {
+    private static final String LOG_FILE_ADDRESS = "src/main/java/Server/logs/ClientHandler_Log.txt";
     public static ArrayList<ClientHandler> clientHandlers = new ArrayList<>();
     private Socket socket;
     private BufferedReader bufferedReader;
@@ -27,6 +30,17 @@ public class ClientHandler {
             System.err.println(errorLog);
             writeLog(errorLog);
             closeEverything(socket , bufferedReader , bufferedWriter);
+        }
+    }
+
+    public void writeLog(String log) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE_ADDRESS, true))) {
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            writer.write("[" + timestamp + "] " + log);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error : while logging inside writeLog function !");
+            e.printStackTrace();
         }
     }
 }
