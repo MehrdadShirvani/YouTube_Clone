a80d632d1cfb89d93678512de61810533ddb296b
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-06-08 21:54:17 +0330

    feat: Channel class. Added Channel class and used data annotations to help object relational mapping

---
+++ YouTube/src/main/java/Shared/Models/Channel.java
@@ -0,0 +1,86 @@
+package Shared.Models;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Channels")
+public class Channel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ChannelId")
+    private Long channelId;
+
+    @Column(name = "Name", unique = true, nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "Description", nullable = false, columnDefinition = "TEXT")
+    private String description;
+    @Column(name = "Location", nullable = false,  length = 50)
+    private String location;
+    @Column(name = "Picture", nullable = false, columnDefinition = "TEXT")
+    private String picture;
+    @Column(name = "CreatedDateTime", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp createdDateTime;
+
+    public Channel()
+    {
+
+    }
+    public Channel(String name, String description, String location, String picture, Timestamp createdDateTime) {
+        this.name = name;
+        this.description = description;
+        this.location = location;
+        this.picture = picture;
+        this.createdDateTime = createdDateTime;
+    }
+    public Long getChannelId() {
+        return channelId;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public Timestamp getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(Timestamp createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+
+
+
+
+}
