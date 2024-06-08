2c3d3e4b678d15be9cd746137033e95e71fbca8b
woxane <mohsen.minavand1384@gmail.com>
2024-06-08 15:35:59 +0330

    implement startServer function in Server class

--- YouTube/src/main/java/Server/Server.java
+++ YouTube/src/main/java/Server/Server.java
@@ -5,6 +5,7 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -18,6 +19,26 @@ public class Server {
             this.serverSocket = new ServerSocket(PORT);
         } catch (IOException e) {
             System.err.println("Error : while Server class constructor started !");
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                String log = "A new client has connected !";
+                System.out.println(log);
+                writeLog(log);
+
+                ClientHandler clientHandler = new ClientHandler(socket);
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+            String errorLog = "Error : IOException error occur while listening for new clients in startServer function";
+            System.err.println(errorLog);
+            writeLog(errorLog);
             throw new RuntimeException(e);
         }
     }
