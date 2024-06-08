5f3e10c79409e9296e6600c64dccfdd07f744b8a
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-06-08 21:56:09 +0330

    feat: VideoCategory class. Added VideoCategory class and used data annotations to help object relational mapping

---
+++ YouTube/src/main/java/Shared/Models/VideoCategory.java
@@ -0,0 +1,48 @@
+package Shared.Models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+public class VideoCategory {
+    @Id
+    @Column(name = "VideoId", nullable = false)
+    private Long videoId;
+
+    @Id
+    @Column(name = "CategoryId", nullable = false)
+    private Integer categoryId;
+
+    @ManyToOne
+    @JoinColumn(name = "VideoId", referencedColumnName = "VideoId", insertable = false, updatable = false)
+    private Video video;
+
+    @ManyToOne
+    @JoinColumn(name = "CategoryId", referencedColumnName = "CategoryId", insertable = false, updatable = false)
+    private Category category;
+
+
+    public Long getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(Long videoId) {
+        this.videoId = videoId;
+    }
+
+    public Integer getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Integer categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public VideoCategory(Long videoId, int categoryId)
+    {
+        this.videoId = videoId;
+        this.categoryId = categoryId;
+    }
+
+}
