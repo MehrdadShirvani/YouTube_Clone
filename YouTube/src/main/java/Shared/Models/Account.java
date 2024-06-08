f91bf3a18082faf808e811347106ae4f93fbc6c4
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-06-08 21:53:39 +0330

    feat: Account class. Added Account class and used data annotations to help object relational mapping

---
+++ YouTube/src/main/java/Shared/Models/Account.java
@@ -0,0 +1,151 @@
+package Shared.Models;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+@Entity
+@Table(name = "Accounts")
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "AccountId")
+    private Long accountId;
+
+    @Column(name = "ChannelId")
+    private Long channelId;
+
+    @Column(name = "FirstName", nullable = false, length = 50)
+    private String firstName;
+
+    @Column(name = "LastName", length = 50)
+    private String lastName;
+
+    @Column(name = "Username", nullable = false, unique = true, length = 100)
+    private String username;
+
+    @Column(name = "Email", nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(name = "Password", nullable = false, columnDefinition = "TEXT")
+    private String password;
+
+    @Column(name = "BirthDate", nullable = false)
+    @Temporal(TemporalType.DATE)
+    private Date birthDate;
+
+    @Column(name = "CreatedDateTime", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp createdDateTime;
+
+    @Column(name = "LastSeenAdDateTime")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Timestamp lastSeenAdDateTime;
+
+    @Column(name = "PremiumExpirationDate")
+    @Temporal(TemporalType.DATE)
+    private Date premiumExpirationDate;
+
+    @Column(name = "PublicKey", nullable = false, unique = true, length = 700)
+    private String publicKey;
+
+
+    @ManyToOne
+    @JoinColumn(name = "ChannelId", insertable = false, updatable = false)
+    private Channel channel;
+
+    public Long getAccountId() {
+        return accountId;
+    }
+    public Long getChannelId() {
+        return channelId;
+    }
+    public void setChannelId(Long channelId) {
+        this.channelId = channelId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Timestamp birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Timestamp getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(Timestamp createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+
+    public Timestamp getLastSeenAdDateTime() {
+        return lastSeenAdDateTime;
+    }
+
+    public void setLastSeenAdDateTime(Timestamp lastSeenAdDateTime) {
+        this.lastSeenAdDateTime = lastSeenAdDateTime;
+    }
+
+    public Date getPremiumExpirationDate() {
+        return premiumExpirationDate;
+    }
+
+    public void setPremiumExpirationDate(Timestamp premiumExpirationDate) {
+        this.premiumExpirationDate = premiumExpirationDate;
+    }
+
+    public String getPublicKey() {
+        return publicKey;
+    }
+
+    public void setPublicKey(String publicKey) {
+        this.publicKey = publicKey;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+}
