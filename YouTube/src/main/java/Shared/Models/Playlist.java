222272a847eb1a55fb22c2c568c1dbc48cca2d36
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-06-08 21:55:05 +0330

    feat: Playlist class. Added Playlist class and used data annotations to help object relational mapping

---
+++ YouTube/src/main/java/Shared/Models/Playlist.java
@@ -0,0 +1,63 @@
+package Shared.Models;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Playlists")
+public class Playlist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "PlaylistId")
+    private Long playlistId;
+
+    @Column(name = "Name", nullable = false, length = 100)
+    private String name;
+
+    @Column(name = "PlaylistTypeId", nullable = false)
+    private Short playlistTypeId;
+
+    @Column(name = "IsPrivate", nullable = false)
+    private Boolean isPrivate;
+
+    @Column(name = "CreatedDateTime", nullable = false)
+    private Timestamp createdDateTime;
+
+    public Long getPlaylistId() {
+        return playlistId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Short getPlaylistTypeId() {
+        return playlistTypeId;
+    }
+
+    public void setPlaylistTypeId(Short playlistTypeId) {
+        this.playlistTypeId = playlistTypeId;
+    }
+
+    public Boolean getPrivate() {
+        return isPrivate;
+    }
+
+    public void setPrivate(Boolean aPrivate) {
+        isPrivate = aPrivate;
+    }
+
+    public Timestamp getCreatedDateTime() {
+        return createdDateTime;
+    }
+
+    public void setCreatedDateTime(Timestamp createdDateTime) {
+        this.createdDateTime = createdDateTime;
+    }
+}
