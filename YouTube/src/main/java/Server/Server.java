e9e3f8eb6d8c5ef661c5b9dbff0737a8122642c5
woxane <mohsen.minavand1384@gmail.com>
2024-06-08 15:15:47 +0330

    implement writeLog function in Server class

--- YouTube/src/main/java/Server/Server.java
+++ YouTube/src/main/java/Server/Server.java
@@ -1,11 +1,16 @@
 package Server;
 
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Server {
     private static final int PORT = 12345;
+    private static final String LOG_FILE_ADDRESS = "src/main/java/Server/logs/log.txt";
     private ServerSocket serverSocket;
 
     public Server() {
@@ -14,6 +19,17 @@ public class Server {
         } catch (IOException e) {
             System.err.println("Error : while Server class constructor started !");
             throw new RuntimeException(e);
+        }
+    }
+
+    public void writeLog(String log) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(LOG_FILE_ADDRESS, true))) {
+            String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+            writer.write("[" + timestamp + "] " + log);
+            writer.newLine();
+        } catch (IOException e) {
+            System.err.println("Error : while logging inside writeLog function !");
+            e.printStackTrace();
         }
     }
 
