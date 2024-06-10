fad9e58368e9efe831114eaa24572d6cf0f8a5ff
woxane <mohsen.minavand1384@gmail.com>
2024-06-10 14:13:48 +0330

    feat : implement getters and setters of username and password for login api method request

--- YouTube/src/main/java/Shared/Api/dto/Body.java
+++ YouTube/src/main/java/Shared/Api/dto/Body.java
@@ -1,4 +1,30 @@
 package Shared.Api.dto;
 
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
 public class Body {
+    private String username;
+    private String password;
+
+
+    public Body() {
+        //TODO
+    }
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
