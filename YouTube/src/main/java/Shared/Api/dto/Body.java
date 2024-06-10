705bb66bd2e4e9be576ea490a7462d9bb69ca430
woxane <mohsen.minavand1384@gmail.com>
2024-06-10 14:31:08 +0330

    feat : implement getters and setters of success , message , username , password , account , channel , category , comment , commentReaction , playlist , reactoin , video , videoCategory , videoView variables inside Body class

--- YouTube/src/main/java/Shared/Api/dto/Body.java
+++ YouTube/src/main/java/Shared/Api/dto/Body.java
@@ -1,17 +1,129 @@
 package Shared.Api.dto;
 
 
+import Shared.Models.*;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
 @JsonSerialize(include= JsonSerialize.Inclusion.NON_NULL)
 public class Body {
+    private boolean success;
+    private String Message;
+
     private String username;
     private String password;
+    private Account account;
+    private Channel channel;
+    private Category category;
+    private Comment comment;
+    private CommentReaction commentReaction;
+    private Playlist playlist;
+    private Reaction reaction;
+    private Video video;
+    private VideoCategory videoCategory;
+    private VideoView videoView;
+
 
 
     public Body() {
         //TODO
     }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Comment getComment() {
+        return comment;
+    }
+
+    public void setComment(Comment comment) {
+        this.comment = comment;
+    }
+
+    public CommentReaction getCommentReaction() {
+        return commentReaction;
+    }
+
+    public void setCommentReaction(CommentReaction commentReaction) {
+        this.commentReaction = commentReaction;
+    }
+
+    public Playlist getPlaylist() {
+        return playlist;
+    }
+
+    public void setPlaylist(Playlist playlist) {
+        this.playlist = playlist;
+    }
+
+    public Reaction getReaction() {
+        return reaction;
+    }
+
+    public void setReaction(Reaction reaction) {
+        this.reaction = reaction;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
+    public VideoCategory getVideoCategory() {
+        return videoCategory;
+    }
+
+    public void setVideoCategory(VideoCategory videoCategory) {
+        this.videoCategory = videoCategory;
+    }
+
+    public VideoView getVideoView() {
+        return videoView;
+    }
+
+    public void setVideoView(VideoView videoView) {
+        this.videoView = videoView;
+    }
+
     public String getUsername() {
         return username;
     }
