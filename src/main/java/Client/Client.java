33665ab0b066b8984d3dd33607672c2951cd36d2
woxane <mohsen.minavand1384@gmail.com>
2024-07-02 11:35:24 +0330

    style(Client): add missing / in getChannelSubscribers endpoint

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -363,7 +363,7 @@ public class Client {
 
 
     public List<Channel> getChannelSubscribers(Long channelId) {
-        String endpoint = "/api/channel/subscribers" + channelId;
+        String endpoint = "/api/channel/subscribers/" + channelId;
         String method = "GET";
         Header requestHeader = new Header(method , endpoint);
         Body requestBody = new Body();
