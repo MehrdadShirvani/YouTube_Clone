27b6d3dfa3f7b2116473b23a4c368397929112dd
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2024-07-03 16:07:12 +0330

    feat(ui): add description video view

--- src/main/java/Client/VideoViewController.java
+++ src/main/java/Client/VideoViewController.java
@@ -3,12 +3,15 @@ package Client;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.effect.Reflection;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.web.WebView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+
+import java.awt.*;
 
 public class VideoViewController {
     @FXML
@@ -17,9 +20,20 @@ public class VideoViewController {
     VBox rightVBox;
     @FXML
     WebView videoWebView;
+    @FXML
+    WebView authorProfile;
+    @FXML
+    WebView commentProfile;
+    @FXML
+    TextField commentTextField;
+    @FXML
+    Button commentButton;
     private Rectangle maskVideoRec;
+    private Rectangle maskProfileRec;
+    private Rectangle maskcommentProfileRec;
 
     public void initialize() {
+        //Pref of main border pain: 1084 * 664
         //bindings
         rightVBox.prefWidthProperty().bind(mainBorderPane.widthProperty().divide(4));
         videoWebView.prefHeightProperty().bind(videoWebView.widthProperty().multiply(0.562));
@@ -33,9 +47,24 @@ public class VideoViewController {
             maskVideoRec.heightProperty().bind(videoWebView.heightProperty());
         });
         videoWebView.getEngine().load("http://example.com");
+        //Mask author profile
+        maskProfileRec = new Rectangle(48, 48);
+        maskProfileRec.setArcWidth(48);
+        maskProfileRec.setArcHeight(48);
+        authorProfile.setClip(maskProfileRec);
+        //Mask comment profile
+        maskcommentProfileRec = new Rectangle(48, 48);
+        maskcommentProfileRec.setArcWidth(48);
+        maskcommentProfileRec.setArcHeight(48);
+        commentProfile.setClip(maskcommentProfileRec);
+
     }
 
     public void hi(ActionEvent event) {
         System.out.println(videoWebView.getWidth());
+    }
+
+    public void commentChanged(KeyEvent keyEvent) {
+        commentButton.setDisable(commentTextField.getText().isBlank());
     }
 }
