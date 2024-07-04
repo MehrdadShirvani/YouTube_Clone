41189c15ee4659807910ee70a075f4f1380bcff7
woxane <mohsen.minavand1384@gmail.com>
2024-07-04 21:28:46 +0330

    feat(client): implement addVideoView function

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1328,6 +1328,29 @@ public class Client {
          return responseBody.isSuccess();
     }
 
+
+    public VideoView addVideoView(VideoView videoView) {
+        String endpoint = "/api/video/add-view";
+        String method = "GET";
+        Header requestHeader = new Header(method , endpoint);
+        Body requestBody =new Body();
+
+        requestBody.setVideoView(videoView);
+
+        Request request = new Request(requestHeader , requestBody);
+
+        sendRequest(request);
+        Response response = handleResponse();
+
+        Body responseBody = response.getBody();
+
+        if (responseBody.isSuccess()) {
+            return responseBody.getVideoView();
+        }
+
+        return null;
+    }
+
     public Account getAccount()
     {
         return account;
