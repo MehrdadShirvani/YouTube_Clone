06bc2cd9584dc986b9886614d71f36dc71f8beaa
woxane <mohsen.minavand1384@gmail.com>
2024-07-04 16:16:56 +0330

    feat(ClientHandler): implement handleAddPlaylist function

--- src/main/java/Server/ClientHandler.java
+++ src/main/java/Server/ClientHandler.java
@@ -1640,6 +1640,34 @@ public class ClientHandler implements Runnable {
     }
 
 
+    public void handleAddPlaylist(Request request) {
+        Response response;
+        Header requestHeader = request.getHeader();
+        Body requestBody = request.getBody();
+        Playlist playlist = requestBody.getPlaylist();
+
+        Body responseBody = new Body();
+
+        if (playlist == null) {
+            responseBody.setSuccess(false);
+            responseBody.setMessage("The playlist object that sent is null !");
+
+            response = new Response(requestHeader , responseBody);
+            sendResponse(response);
+            return;
+        }
+
+        Playlist addedPlaylist = DatabaseManager.addPlaylist(playlist);
+
+        responseBody.setSuccess(true);
+        responseBody.setMessage("200 Ok");
+        responseBody.setPlaylist(addedPlaylist);
+
+        response = new Response(requestHeader , responseBody);
+        sendResponse(response);
+    }
+
+
     public HashMap<String , Double> dataConversion(HashMap<String , Integer>  data, double percentage) throws Exception {
         HashMap<String, Double> result = new HashMap<>();
         for (Map.Entry<String, Integer> set : data.entrySet()) {
