c26a55bb134b69cad22eb72c5b84bf44f974cd53
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-07-04 21:43:56 +0330

    feat(MedialServer): added uploading and downloading videos

--- src/main/java/Server/MediaServer.java
+++ src/main/java/Server/MediaServer.java
@@ -7,23 +7,29 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.file.Files;
+import java.util.concurrent.Executors;
 
 public class MediaServer {
 
     private static final int PORT = 2131;
-    public static void run()
-    {
-        HttpServer server = null;
+
+    public static void run() {
         try {
-            server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            HttpServer server = HttpServer.create(new InetSocketAddress(PORT), 0);
+            server.createContext("/video", new VideoHandler());
+            server.createContext("/image", new PhotoHandler());
+            server.createContext("/download", new DownloadHandler());
+            server.createContext("/upload", new UploadHandler());
+            server.setExecutor(Executors.newCachedThreadPool());
+            server.start();
+            System.out.println("Server started on port " + PORT);
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Server could not start", e);
         }
-        server.createContext("/video", new VideoHandler());
-        server.createContext("/image", new PhotoHandler());
-        server.setExecutor(null);
-        server.start();
-        System.out.println("Server started on port " + PORT);
+    }
+
+    private static File getVideoFilePath(long videoId) {
+        return new File("src/main/resources/Server/Videos/" + videoId + ".mp4");
     }
 
     static class PhotoHandler implements HttpHandler {
@@ -32,27 +38,26 @@ public class MediaServer {
         public void handle(HttpExchange exchange) throws IOException {
             String requestPath = exchange.getRequestURI().getPath();
             String[] parts = requestPath.split("/");
-            if(parts.length != 3)
-            {
-                exchange.sendResponseHeaders(400, 0);
+
+            if (parts.length != 3) {
+                exchange.sendResponseHeaders(400, -1);
                 exchange.close();
                 return;
             }
 
             String photoId = parts[2];
 
-
             if ("HEAD".equalsIgnoreCase(exchange.getRequestMethod())) {
                 handleHeadRequest(exchange, photoId);
             } else if ("GET".equalsIgnoreCase(exchange.getRequestMethod())) {
                 handleGetRequest(exchange, photoId);
             } else {
-                exchange.sendResponseHeaders(405, 0);
+                exchange.sendResponseHeaders(405, -1);
                 exchange.close();
             }
         }
 
-        public byte[] getPhotoBytes(String photoId) {
+        private byte[] getPhotoBytes(String photoId) {
             File file = new File("src/main/resources/Server/Images/" + photoId + ".jpg");
             try {
                 return Files.readAllBytes(file.toPath());
@@ -64,7 +69,7 @@ public class MediaServer {
         private void handleHeadRequest(HttpExchange exchange, String photoId) throws IOException {
             byte[] photoBytes = getPhotoBytes(photoId);
             if (photoBytes == null) {
-                exchange.sendResponseHeaders(404, 0);
+                exchange.sendResponseHeaders(404, -1);
                 exchange.close();
                 return;
             }
@@ -74,28 +79,15 @@ public class MediaServer {
             exchange.close();
         }
 
-
         private void handleGetRequest(HttpExchange exchange, String photoId) throws IOException {
-            new Thread(() -> {
-                try {
-                    byte[] photoBytes = getPhotoBytes(photoId);
-                    if (photoBytes == null) {
-                        exchange.sendResponseHeaders(404, 0);
-                        exchange.close();
-                        return;
-                    }
+            byte[] photoBytes = getPhotoBytes(photoId);
+            if (photoBytes == null) {
+                exchange.sendResponseHeaders(404, -1);
+                exchange.close();
+                return;
+            }
 
-                    sendFullResponse(exchange, photoBytes);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    try {
-                        exchange.sendResponseHeaders(500, 0);
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    }
-                    exchange.close();
-                }
-            }).start();
+            sendFullResponse(exchange, photoBytes);
         }
 
         private void sendFullResponse(HttpExchange exchange, byte[] photoBytes) throws IOException {
@@ -105,50 +97,109 @@ public class MediaServer {
 
             try (OutputStream os = exchange.getResponseBody()) {
                 os.write(photoBytes);
+            } finally {
+                exchange.close();
             }
-
-            exchange.close();
         }
+    }
+    static class DownloadHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String requestPath = exchange.getRequestURI().getPath();
+                String[] parts = requestPath.split("/");
 
+                if (parts.length != 3) {
+                    exchange.sendResponseHeaders(400, -1);
+                    exchange.close();
+                    return;
+                }
+
+                Long videoId = Long.parseLong(parts[2]);
+                File videoFile = getVideoFilePath(videoId);
+
+                if (videoFile.exists() && videoFile.isFile()) {
+                    try (InputStream is = new FileInputStream(videoFile)) {
+                        exchange.sendResponseHeaders(200, videoFile.length());
+                        byte[] buffer = new byte[1024];
+                        int bytesRead;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            exchange.getResponseBody().write(buffer, 0, bytesRead);
+                        }
+                    } catch (IOException e) {
+                        exchange.sendResponseHeaders(500, -1); // Internal server error
+                    }
+                }
+                exchange.close();
+            }
+        }
+    }
+
+    static class UploadHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            if ("POST".equals(exchange.getRequestMethod())) {
+                String contentType = exchange.getRequestHeaders().getFirst("Content-Type");
+                if(contentType == null ||contentType.split("[|]").length != 2 ||contentType.split("[|]")[0].equalsIgnoreCase("video/mp4"))
+                {
+                    exchange.sendResponseHeaders(400, 0);
+                    exchange.close();
+                    return;
+                }
+                long videoId = Long.parseLong(contentType.split("[|]")[1]);
+
+                String uploadPath = "src/main/resources/Server/Videos/" + videoId + ".mp4";
+                File videoFile = new File(uploadPath);
+                try(InputStream is =  exchange.getRequestBody();
+                FileOutputStream fos = new FileOutputStream(videoFile))
+                {
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = is.read(buffer)) != -1) {
+                        fos.write(buffer, 0, bytesRead);
+                    }
+                }
+                exchange.sendResponseHeaders(200, 0);
+                exchange.close();
+            }
+        }
     }
     static class VideoHandler implements HttpHandler {
+
         @Override
         public void handle(HttpExchange exchange) throws IOException {
             String requestPath = exchange.getRequestURI().getPath();
             String[] parts = requestPath.split("/");
-            if(parts.length != 3)
-            {
-                exchange.sendResponseHeaders(400, 0);
-                exchange.close();
-                return;
-            }
-            long videoId;
-            try {
-                videoId = Long.parseLong(parts[2]);
-            }
-            catch(Exception ignore) {
-                exchange.sendResponseHeaders(400, 0);
+
+            if (parts.length != 3) {
+                exchange.sendResponseHeaders(400, -1);
                 exchange.close();
                 return;
             }
 
+            long videoId;
+            try {
+                videoId = Long.parseLong(parts[2]);
+            } catch (NumberFormatException e) {
+                exchange.sendResponseHeaders(400, -1);
+                exchange.close();
+                return;
+            }
 
             if ("HEAD".equalsIgnoreCase(exchange.getRequestMethod())) {
                 handleHeadRequest(exchange, videoId);
             } else if ("GET".equalsIgnoreCase(exchange.getRequestMethod())) {
                 handleGetRequest(exchange, videoId);
             } else {
-                exchange.sendResponseHeaders(405, 0);
+                exchange.sendResponseHeaders(405, -1);
                 exchange.close();
             }
         }
 
         private void handleHeadRequest(HttpExchange exchange, long videoId) throws IOException {
-
             File videoFile = getVideoFilePath(videoId);
-
             if (!videoFile.exists() || !videoFile.canRead()) {
-                exchange.sendResponseHeaders(404, 0);
+                exchange.sendResponseHeaders(404, -1);
                 exchange.close();
                 return;
             }
@@ -160,37 +211,22 @@ public class MediaServer {
             exchange.close();
         }
 
-        private File getVideoFilePath(long videoId)
-        {
-            return new File("src/main/resources/Server/Videos/" + videoId + ".mp4");
-        }
+
 
         private void handleGetRequest(HttpExchange exchange, long videoId) throws IOException {
-            new Thread(() -> {
-                try {
-                    File videoFile = getVideoFilePath(videoId);
-                    if (!videoFile.exists() || !videoFile.canRead()) {
-                        exchange.sendResponseHeaders(404, 0);
-                        exchange.close();
-                        return;
-                    }
+            File videoFile = getVideoFilePath(videoId);
+            if (!videoFile.exists() || !videoFile.canRead()) {
+                exchange.sendResponseHeaders(404, -1);
+                exchange.close();
+                return;
+            }
 
-                    String rangeHeader = exchange.getRequestHeaders().getFirst("Range");
-                    if (rangeHeader == null) {
-                        sendFullResponse(exchange, videoFile);
-                    } else {
-                        sendPartialResponse(exchange, videoFile, rangeHeader);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    try {
-                        exchange.sendResponseHeaders(500, 0);
-                    } catch (IOException ioException) {
-                        ioException.printStackTrace();
-                    }
-                    exchange.close();
-                }
-            }).start();
+            String rangeHeader = exchange.getRequestHeaders().getFirst("Range");
+            if (rangeHeader == null) {
+                sendFullResponse(exchange, videoFile);
+            } else {
+                sendPartialResponse(exchange, videoFile, rangeHeader);
+            }
         }
 
         private void sendFullResponse(HttpExchange exchange, File videoFile) throws IOException {
@@ -201,15 +237,14 @@ public class MediaServer {
 
             try (OutputStream responseBody = exchange.getResponseBody();
                  FileInputStream fileInputStream = new FileInputStream(videoFile)) {
-
                 byte[] buffer = new byte[4096];
                 int bytesRead;
                 while ((bytesRead = fileInputStream.read(buffer)) != -1) {
                     responseBody.write(buffer, 0, bytesRead);
                 }
+            } finally {
+                exchange.close();
             }
-
-            exchange.close();
         }
 
         private void sendPartialResponse(HttpExchange exchange, File videoFile, String rangeHeader) throws IOException {
@@ -226,7 +261,7 @@ public class MediaServer {
             exchange.getResponseHeaders().set("Content-Type", "video/mp4");
             exchange.getResponseHeaders().set("Content-Range", "bytes " + start + "-" + end + "/" + fileSize);
             exchange.getResponseHeaders().set("Content-Length", String.valueOf(contentLength));
-            exchange.sendResponseHeaders(206, contentLength); // 206 for Partial Content
+            exchange.sendResponseHeaders(206, contentLength);
 
             try (OutputStream responseBody = exchange.getResponseBody();
                  FileInputStream fileInputStream = new FileInputStream(videoFile)) {
@@ -239,9 +274,9 @@ public class MediaServer {
                     responseBody.write(buffer, 0, bytesRead);
                     bytesToSend -= bytesRead;
                 }
+            } finally {
+                exchange.close();
             }
-
-            exchange.close();
         }
     }
 }
