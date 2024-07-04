37b088b9d604ec1e044861081cd4960a88fb288f
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2024-07-04 21:34:03 +0330

    feat(ui): style applied for video play + shortcuts

--- src/main/java/Client/VideoViewController.java
+++ src/main/java/Client/VideoViewController.java
@@ -3,18 +3,22 @@ package Client;
 import Shared.Models.Video;
 import Shared.Utils.DateFormats;
 import javafx.application.Platform;
+import javafx.concurrent.Worker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
-
+import javafx.scene.control.ScrollPane;
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
@@ -47,12 +51,15 @@ public class VideoViewController {
     TextField commentTextField;
     @FXML
     Button commentButton;
+    @FXML
+    ScrollPane leftScrollPane;
     private Rectangle maskVideoRec;
     private Rectangle maskProfileRec;
     private Rectangle maskcommentProfileRec;
+    private WebEngine engine;
 
     public void initialize() {
-        //Pref of main border pain: 1084 * 664
+        //Pref of main border pane: 1084 * 664
         //bindings
         rightVBox.prefWidthProperty().bind(mainBorderPane.widthProperty().divide(4));
         videoWebView.prefHeightProperty().bind(videoWebView.widthProperty().multiply(0.562));
@@ -61,6 +68,7 @@ public class VideoViewController {
             maskVideoRec = new Rectangle(videoWebView.getWidth(), videoWebView.getWidth() * 0.562);
             maskVideoRec.setArcWidth(25);
             maskVideoRec.setArcHeight(25);
+            maskVideoRec.setFill(Paint.valueOf("#303030"));
             videoWebView.setClip(maskVideoRec);
             maskVideoRec.widthProperty().bind(videoWebView.widthProperty());
             maskVideoRec.heightProperty().bind(videoWebView.heightProperty());
@@ -76,6 +84,22 @@ public class VideoViewController {
         maskcommentProfileRec.setArcWidth(48);
         maskcommentProfileRec.setArcHeight(48);
         commentProfile.setClip(maskcommentProfileRec);
+        //Arrow keys assign
+        leftScrollPane.addEventFilter(KeyEvent.KEY_PRESSED, event -> {
+            if (event.getCode() == KeyCode.DOWN || event.getCode() == KeyCode.UP|| event.getCode() == KeyCode.LEFT||  event.getCode() == KeyCode.RIGHT || event.getCode() == KeyCode.SPACE) {
+                if( event.getCode() == KeyCode.LEFT)
+                    engine.executeScript("player.forward(-10);");
+                if( event.getCode() == KeyCode.RIGHT)
+                    engine.executeScript("player.forward(10);");
+                if( event.getCode() == KeyCode.UP)
+                    engine.executeScript("player.increaseVolume(0.1);");
+                if( event.getCode() == KeyCode.DOWN)
+                    engine.executeScript("player.decreaseVolume(-0.1);");
+                if( event.getCode() == KeyCode.SPACE)
+                    engine.executeScript("player.togglePlay();");
+                event.consume(); // Prevent default
+            }
+        });
     }
     public void setVideo(Video video)
     {
@@ -108,13 +132,16 @@ public class VideoViewController {
         try {
             Path path = new File("src/main/resources/Client/video-player.html").toPath();
             String htmlContent = new String(Files.readAllBytes(path));
-            videoWebView.getEngine().loadContent(htmlContent.replace("@id", video.getVideoId()+""));
+//            videoWebView.getEngine().loadContent(htmlContent.replace("@id", video.getVideoId()+""));
+            engine = videoWebView.getEngine();
+            engine.loadContent(htmlContent);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
     public void hi(ActionEvent event) {
         System.out.println(videoWebView.getWidth());
+        engine.executeScript("player.play()");
     }
 
     public void commentChanged(KeyEvent keyEvent) {
