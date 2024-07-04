e1c49b44f2de92a90f267a8eb215fb7a7c304273
woxane <mohsen.minavand1384@gmail.com>
2024-07-04 15:38:04 +0330

    feat(ClientHandler): implement handleAddVideo function

--- src/main/java/Server/ClientHandler.java
+++ src/main/java/Server/ClientHandler.java
@@ -1457,6 +1457,34 @@ public class ClientHandler implements Runnable {
     }
 
 
+    public void handleAddVideo(Request request) {
+        Response response;
+        Header requestHeader = request.getHeader();
+        Body requestBody = request.getBody();
+        Video video = requestBody.getVideo();
+
+        Body responseBody = new Body();
+
+        if (video == null) {
+            responseBody.setSuccess(false);
+            responseBody.setMessage("The video object that sent is null !");
+
+            response = new Response(requestHeader , responseBody);
+            sendResponse(response);
+            return;
+        }
+
+        Video addedVideo = DatabaseManager.addVideo(video);
+
+        responseBody.setSuccess(true);
+        responseBody.setMessage("200 Ok");
+        responseBody.setVideo(addedVideo);
+
+        response = new Response(requestHeader , responseBody);
+        sendResponse(response);
+    }
+
+
     public HashMap<String , Double> dataConversion(HashMap<String , Integer>  data, double percentage) throws Exception {
         HashMap<String, Double> result = new HashMap<>();
         for (Map.Entry<String, Integer> set : data.entrySet()) {
