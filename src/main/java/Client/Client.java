1c3f8ddde65e1e3fafe59f95c9410e803cb6e0a4
woxane <mohsen.minavand1384@gmail.com>
2024-07-04 12:41:08 +0330

    faet(Client): implement isChannelNameUnique function

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -1106,6 +1106,26 @@ public class Client {
         return null;
     }
 
+    public Boolean isChannelNameUnique() {
+        String endpoint = "/api/isUnique/channelName";
+        String method = "GET";
+        Header requestHeader = new Header(method , endpoint);
+        Body requestBody = new Body();
+
+        Request request = new Request(requestHeader , requestBody);
+
+        sendRequest(request);
+        Response response = handleResponse();
+
+        Body responseBody = response.getBody();
+
+        if (responseBody.isSuccess()) {
+            return responseBody.isChannelNameUnique();
+        }
+
+        return null;
+    }
+
     public Account getAccount()
     {
         return account;
