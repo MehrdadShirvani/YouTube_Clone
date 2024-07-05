9f961323d4891625aaf224673159b886324311d6
woxane <mohsen.minavand1384@gmail.com>
2024-07-05 20:16:53 +0330

    feat(ClientHandler): implement handleAddVideoPlaylists function

--- src/main/java/Server/ClientHandler.java
+++ src/main/java/Server/ClientHandler.java
@@ -1902,6 +1902,31 @@ public class ClientHandler implements Runnable {
     }
 
 
+    public void handleAddVideoPlaylists(Request request) {
+        Response response;
+        Header requestHeader = request.getHeader();
+        Body requestBody = request.getBody();
+        Long videoId = requestBody.getVideoId();
+        List<Long> playlistIds = requestBody.getPlaylistIds();
+
+        Body responseBody = new Body();
+
+        if (videoId == null | playlistIds == null) {
+            sendNullErrorResponse(requestHeader , "The videoId or playlistIds that sent is null");
+            return;
+        }
+
+        List<VideoPlaylist> videoPlaylists = DatabaseManager.addVideoPlaylists(videoId , playlistIds);
+
+        responseBody.setSuccess(true);
+        responseBody.setMessage("200 Ok");
+        responseBody.setVideoPlaylists(videoPlaylists);
+
+        response = new Response(requestHeader , responseBody);
+        sendResponse(response);
+    }
+
+
     public HashMap<String , Double> dataConversion(HashMap<String , Integer>  data, double percentage) throws Exception {
         HashMap<String, Double> result = new HashMap<>();
         for (Map.Entry<String, Integer> set : data.entrySet()) {
