eb49b984c0d336a97be53c546ca2d9c7b24f61ed
woxane <mohsen.minavand1384@gmail.com>
2024-07-05 13:55:21 +0330

    feat(RetryPageController): implement retry button action

--- src/main/java/Client/RetryPageController.java
+++ src/main/java/Client/RetryPageController.java
@@ -1,8 +1,21 @@
 package Client;
 
+import Shared.Models.Account;
 import javafx.event.ActionEvent;
+
+import java.io.IOException;
 
 public class RetryPageController {
     public void retryAction(ActionEvent actionEvent) {
+        try {
+            Client newClient = new Client();
+            Account account = YouTube.client.getAccount();
+
+            newClient.sendLoginRequest(account.getUsername() , account.getPassword());
+            YouTube.client = newClient;
+
+        } catch (IOException ignored) {
+            //TODO Ehsan: make pop up error to show that still there is no connection
+        }
     }
 }
