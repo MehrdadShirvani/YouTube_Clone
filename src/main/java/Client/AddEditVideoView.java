d76ebb081a28a094912096e5e55788f0c862c4ee
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-07-05 12:04:38 +0330

    feat(addEditVideoView): Added the basic addEditVideoView page

---
+++ src/main/java/Client/AddEditVideoView.java
@@ -0,0 +1,122 @@
+package Client;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.net.*;
+
+public class AddEditVideoView {
+    ExecutorService executorService = Executors.newCachedThreadPool();
+    public TextField TxtName;
+    public TextField TxtDesc;
+    public CheckBox CBIsPrivate;
+    public CheckBox CBIsAgeRestricted;
+    public CheckBox CBIsShort;
+    public ImageView ImgThumbnail;
+    File videoFile;
+    File pictureFile;
+    public void chooseFile(ActionEvent actionEvent)
+    {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Video File");
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("MP4 files (*.mp4)", "*.mp4");
+        fileChooser.getExtensionFilters().add(filter);
+        Stage dialogStage = new Stage();
+        videoFile = fileChooser.showOpenDialog(dialogStage);
+
+        if (videoFile != null) {
+            //TODO -> setting image to show the default thumbnail
+            //TODO -> setting duration
+            //
+        }
+    }
+
+    private void uploadTheVideo(File selectedFile) {
+        executorService.submit(() -> {
+            try {
+
+                URL url = new URL("http://localhost:2131/upload");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                connection.setRequestMethod("POST");
+                connection.setDoOutput(true);
+                connection.setRequestProperty("Content-Type", "video/mp4|" + 1);
+
+                try (OutputStream outputStream = connection.getOutputStream();
+                     FileInputStream fileInputStream = new FileInputStream(selectedFile)) {
+
+                    byte[] buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = fileInputStream.read(buffer)) != -1) {
+                        outputStream.write(buffer, 0, bytesRead);
+                    }
+                    outputStream.flush();
+
+                    int responseCode = connection.getResponseCode();
+                    System.out.println("Response Code: " + responseCode);
+
+                } finally {
+                    connection.disconnect();
+                }
+            }catch (Exception ex)
+            {
+
+            }
+        });
+    }
+
+    public void choosePictureFile(ActionEvent actionEvent) throws FileNotFoundException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Video File");
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.jpg");
+        fileChooser.getExtensionFilters().add(filter);
+        Stage dialogStage = new Stage();
+        pictureFile = fileChooser.showOpenDialog(dialogStage);
+        if(pictureFile != null)
+        {
+            Image image = new Image(new FileInputStream(pictureFile));
+            ImgThumbnail.setImage(image);
+        }
+    }
+
+    public void uploadVideo(ActionEvent actionEvent) {
+        if(TxtName.getText().isBlank() ||  TxtDesc.getText().isBlank())
+        {
+            //TODO Ehsan Show error
+            return;
+        }
+
+        if(videoFile == null)
+        {
+            //TODO Ehsan Show error
+            return;
+        }
+        if(pictureFile == null)
+        {
+            //TODO Ehsan Show error
+            return;
+        }
+
+        //TODO Mohsen
+        //create video using the constructor
+        //set the duration of the video
+        //call the api and post the video and get the new videoId
+
+        //TODO Mehrdad
+        //Upload the video and files
+        //handle edit mode
+    }
+
+    public void setDefaultThumbnail(ActionEvent actionEvent)
+    {
+
+    }
+}
