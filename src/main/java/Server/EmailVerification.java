c080e5a7458a25edd732c38a8ca1ad3db287b729
woxane <mohsen.minavand1384@gmail.com>
2024-07-05 18:31:47 +0330

    feat(EmailVerification): implement verificationToekn function and utilize it in constructor

--- src/main/java/Server/EmailVerification.java
+++ src/main/java/Server/EmailVerification.java
@@ -4,17 +4,22 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.UUID;
 
 public class EmailVerification {
     private final String FILE_PATH = "src/main/java/Server/.EmailData.txt";
-    private String email;
+    private String senderEmail;
     private String password;
+    private final String recipientsEmail;
+    private final String token;
 
-    public EmailVerification() {
+    public EmailVerification(String recipientsEmail) {
+        this.recipientsEmail = recipientsEmail;
+        this.token = verificationToken();
         readConfigFile();
     }
 
-    public void readConfigFile() {
+    private void readConfigFile() {
         try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
             String line;
             while ((line = br.readLine()) != null) {
@@ -23,7 +28,7 @@ public class EmailVerification {
                     String key = parts[0].trim();
                     String value = parts[1].trim();
                     if (key.equals("email")) {
-                        this.email = value;
+                        this.senderEmail= value;
                     } else if (key.equals("password")) {
                         this.password = value;
                     }
@@ -32,5 +37,9 @@ public class EmailVerification {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private String verificationToken() {
+        return UUID.randomUUID().toString();
     }
 }
