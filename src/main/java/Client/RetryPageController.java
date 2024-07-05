44c95d467cb6b2c044d7b4c0778fec767e35e155
woxane <mohsen.minavand1384@gmail.com>
2024-07-05 14:32:50 +0330

    feat(Client): changeScene after making a connection

--- src/main/java/Client/RetryPageController.java
+++ src/main/java/Client/RetryPageController.java
@@ -7,12 +7,22 @@ import java.io.IOException;
 
 public class RetryPageController {
     public void retryAction(ActionEvent actionEvent) {
+        //TODO Ehsan: make a loading circle while clicking on retry button
         try {
             Client newClient = new Client();
             Account account = YouTube.client.getAccount();
 
-            newClient.sendLoginRequest(account.getUsername() , account.getPassword());
-            YouTube.client = newClient;
+            boolean isLoggedIn = newClient.sendLoginRequest(account.getUsername() , account.getPassword());
+
+            if (isLoggedIn) {
+                YouTube.client = newClient;
+
+                String homeViewName = "home-view.fxml";
+                YouTube.changeScene(homeViewName);
+            } else {
+                String homeViewName = "login-view.fxml";
+                YouTube.changeScene(homeViewName);
+            }
 
         } catch (IOException ignored) {
             //TODO Ehsan: make pop up error to show that still there is no connection
