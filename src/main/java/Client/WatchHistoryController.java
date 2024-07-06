394249ccd150ebd11f64ba5e34bc8b44888dee18
woxane <mohsen.minavand1384@gmail.com>
2024-07-06 16:14:16 +0330

    feat(WatchHistoryController): implement setVideo function

--- src/main/java/Client/WatchHistoryController.java
+++ src/main/java/Client/WatchHistoryController.java
@@ -1,4 +1,38 @@
 package Client;
 
+import Shared.Models.Video;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.FlowPane;
+
+import java.io.IOException;
+import java.util.List;
+
 public class WatchHistoryController {
+    @FXML
+    public FlowPane centerFlow;
+    private final int PER_PAGE = 5;
+    private int pageNumber = 1;
+
+
+    public void setVideo(HomeController homeController) {
+        List<Video> watchHistoryVideos = YouTube.client.getWatchHistory(PER_PAGE , pageNumber);
+        pageNumber++;
+
+        for (Video video : watchHistoryVideos) {
+            FXMLLoader fxmlLoader = new FXMLLoader(HomeController.class.getResource("small-watch-history-view.fxml"));
+            Parent smallVideo = null;
+
+            try {
+                smallVideo = fxmlLoader.load();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            SmallWatchHistoryController smallWatchHistoryController = fxmlLoader.getController();
+            smallWatchHistoryController.setVideo(video, homeController);
+            centerFlow.getChildren().add(smallVideo);
+        }
+    }
 }
