2f259143d37d0e4a1283af28023659c0c4b40e6b
woxane <mohsen.minavand1384@gmail.com>
2024-07-07 21:11:27 +0330

    feat(EmailVerificationCOntroller): implement initialize function

--- src/main/java/Client/EmailVerificationController.java
+++ src/main/java/Client/EmailVerificationController.java
@@ -1,4 +1,20 @@
 package Client;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class EmailVerificationController {
+    @FXML
+    private Label emailSentLabel;
+
+    @FXML
+    private TextField tokenField;
+
+    public void initialize() {
+        String email = YouTube.client.getAccount().getEmail();
+        String emailSentText = "We've sent an token to " + email;
+
+        emailSentLabel.setText(emailSentText);
+    }
 }
