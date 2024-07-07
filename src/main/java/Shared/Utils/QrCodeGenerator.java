529b25fdedc85c65fdff211be8100c1832854ef7
woxane <mohsen.minavand1384@gmail.com>
2024-07-07 19:39:59 +0330

    feat(QrCodeGenerator): implement generateQrCode function

---
+++ src/main/java/Shared/Utils/QrCodeGenerator.java
@@ -0,0 +1,28 @@
+package Shared.Utils;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.EncodeHintType;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
+public class QrCodeGenerator {
+    private static final String FILE_PATH = "src/main/java/Client/.cache/.QrCode.png";
+
+    public static void generateQRCodeImage(String barcodeText) throws WriterException, IOException {
+        QRCodeWriter barcodeWriter = new QRCodeWriter();
+        Map<EncodeHintType, Object> hints = new HashMap<>();
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        BitMatrix bitMatrix = barcodeWriter.encode(barcodeText, BarcodeFormat.QR_CODE, 200, 200, hints);
+
+        Path path = FileSystems.getDefault().getPath(FILE_PATH);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+}
