af3ab08e926e1c1726b6233e2480e8d5a72155b1
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-07-07 21:29:21 +0330

    feat(AddEditPlaylistController): Added addEditPlaylistView and its controller

---
+++ src/main/java/Client/AddEditPlaylistController.java
@@ -0,0 +1,66 @@
+package Client;
+
+import Shared.Models.Playlist;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+import java.util.Objects;
+
+public class AddEditPlaylistController {
+    public TextField TxtName;
+    public CheckBox CBIsPrivate;
+    public CheckBox CBIsCollaborative;
+    public Button SubmitBtn;
+    private Playlist playlist;
+
+    public void setPlaylist(Playlist playlist)
+    {
+        this.playlist = playlist;
+        TxtName.setText(playlist.getName());
+        //Normal 1
+        //Collaborative 2
+        //Watch Later
+        CBIsPrivate.setSelected(playlist.getPrivate());
+        CBIsCollaborative.setSelected(playlist.getPlaylistTypeId() == 2);
+        CBIsCollaborative.setDisable(playlist.getPrivate());
+    }
+    public void submitAction(ActionEvent actionEvent)
+    {
+        if(TxtName.getText().isBlank())
+        {
+            //TODO Ehsan show error
+            return;
+        }
+        if(Objects.equals(TxtName.getText(), "Watch Later"))
+        {
+            //TODO Ehsan show error
+            return;
+        }
+
+        if(playlist == null)
+        {
+           Playlist newPlaylist =  YouTube.client.addPlaylist(new Playlist(TxtName.getText(),CBIsCollaborative.isSelected()?(short)2:(short)1, CBIsPrivate.isSelected()));
+            //TODO Ehsan -> get back to page
+        }
+        else
+        {
+            playlist.setName(TxtName.getText());
+            playlist.setPlaylistTypeId(CBIsCollaborative.isSelected()?(short)2:(short)1);
+            playlist.setPrivate(CBIsPrivate.isSelected());
+            playlist = YouTube.client.editPlaylist(playlist);
+            //TODO Ehsan -> get back to page
+        }
+
+    }
+
+    public void cbPrivateAction(ActionEvent actionEvent)
+    {
+        CBIsCollaborative.setDisable(playlist.getPrivate());
+        if(CBIsCollaborative.isDisable())
+        {
+            CBIsCollaborative.setSelected(false);
+        }
+    }
+}
