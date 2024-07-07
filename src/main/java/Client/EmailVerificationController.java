83ff0a6aff6c535a1f7f0599eada462ea4ab0f74
woxane <mohsen.minavand1384@gmail.com>
2024-07-07 21:16:53 +0330

    feat(EmailVerificationController): implement verifyKey fucntion

--- src/main/java/Client/EmailVerificationController.java
+++ src/main/java/Client/EmailVerificationController.java
@@ -4,6 +4,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
+import java.awt.event.ActionEvent;
+
 public class EmailVerificationController {
     @FXML
     private Label emailSentLabel;
@@ -16,5 +18,21 @@ public class EmailVerificationController {
         String emailSentText = "We've sent an token to " + email;
 
         emailSentLabel.setText(emailSentText);
+    }
+
+    public void verifyKey(ActionEvent actionEvent) {
+        String inputToken = tokenField.getText();
+        String originalToken = YouTube.client.verifyEmail();
+
+        if (inputToken == null) {
+            //TODO Ehsan : make a pop up error that input token can't be null
+            return;
+        }
+
+        if (inputToken.equals(originalToken)) {
+            //TODO : change to next scene
+        } else {
+            //TODO Ehsan : pop up error that it's not equals
+        }
     }
 }
