2d8d9c14f6d2db7f61239e68291ce443b6dde04f
woxane <mohsen.minavand1384@gmail.com>
2024-07-07 17:47:27 +0330

    feat(twoFactorAuthentication): implement the class their constructors with generateSecretKey function

--- src/main/java/Server/twoFactorAuthentication.java
+++ src/main/java/Server/twoFactorAuthentication.java
@@ -1,2 +1,26 @@
-package Server;public class twoFactorAuthentication {
+package Server;
+
+
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+public class twoFactorAuthentication {
+
+    private GoogleAuthenticator googleAuthenticator;
+    private String secretKey;
+
+    public twoFactorAuthentication() {
+        googleAuthenticator = new GoogleAuthenticator();
+        generateSecretKey();
+    }
+
+    public twoFactorAuthentication(String secretKey) {
+        googleAuthenticator = new GoogleAuthenticator();
+        this.secretKey = secretKey;
+    }
+
+
+    public void generateSecretKey() {
+        GoogleAuthenticatorKey key = googleAuthenticator.createCredentials();
+        this.secretKey = key.getKey();
+    }
 }
