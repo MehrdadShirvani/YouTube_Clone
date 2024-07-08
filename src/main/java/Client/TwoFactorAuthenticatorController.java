5ea10b663532b1927d19a650357243f2bec4ee87
woxane <mohsen.minavand1384@gmail.com>
2024-07-08 16:43:30 +0330

    feat(TwoFactorAuthenticator): implement skeletion useEmailButton , loginButton function

---
+++ src/main/java/Client/TwoFactorAuthenticatorController.java
@@ -0,0 +1,16 @@
+package Client;
+
+import java.awt.event.ActionEvent;
+
+public class TwoFactorAuthenticatorController {
+    public void useEmailButton(ActionEvent actionEvent) {
+        //TODO change scene to useEmailButton
+
+    }
+
+
+    public void loginButton(ActionEvent actionEvent) {
+        //TODO login
+
+    }
+}
