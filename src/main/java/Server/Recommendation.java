989f6bd414f2a62ad909c0dfd334a9e58b66831b
woxane <mohsen.minavand1384@gmail.com>
2024-07-08 10:19:22 +0330

    feat(Recommendation): implement interestedCategroeis function

--- src/main/java/Server/Recommendation.java
+++ src/main/java/Server/Recommendation.java
@@ -1,5 +1,6 @@
 package Server;
 
+import Server.Database.DatabaseManager;
 import Shared.Models.Category;
 
 import java.util.HashMap;
@@ -14,10 +15,50 @@ public class Recommendation {
         this.channelId = channelId;
     }
 
-    private HashMap<Category, Double> dataConversion(HashMap<Category, Integer>  data, double percentage) throws Exception {
-        HashMap<Category , Double> result = new HashMap<>();
-        for (Map.Entry<Category , Integer> set : data.entrySet()) {
-            result.put(set.getKey(), (set.getValue() / (double) data.get("sum")) * percentage);
+
+    public HashMap<String, Double> interestedCategories() {
+        try {
+            Date endDate = new Date();
+
+            Date startDate24HoursAgo = calculateStartDate(endDate, Calendar.HOUR, -24);
+            HashMap<String , Long> daily = DatabaseManager.dataAnalysis(this.channelId , startDate24HoursAgo , endDate);
+            HashMap<String , Double> normalizedDailyValue = dataConversion(daily , 0.5);
+
+            Date startDate7DaysAgo = calculateStartDate(endDate, Calendar.DAY_OF_MONTH, -7);
+            HashMap<String , Long> weekly = DatabaseManager.dataAnalysis(this.channelId , startDate7DaysAgo , endDate);
+            HashMap<String , Double> normalizedWeeklyValue = dataConversion(weekly , 0.2);
+
+            Date startDate1MonthAgo = calculateStartDate(endDate, Calendar.MONTH, -1);
+            HashMap<String , Long> monthly = DatabaseManager.dataAnalysis(this.channelId , startDate1MonthAgo , endDate);
+            HashMap<String , Double> normalizedMonthlyValue = dataConversion(monthly , 0.2);
+
+            //TODO : put null for startData and endData param after database got updated
+            HashMap<String , Long> allTime = DatabaseManager.dataAnalysis(this.channelId , startDate1MonthAgo , endDate);
+            HashMap<String , Double> normalizedAllTimeValue = dataConversion(allTime, 0.1);
+
+            HashMap<String , Double> result = new HashMap<>();
+
+            for (Map.Entry<String, Double> entry : normalizedAllTimeValue.entrySet()) {
+                String key = entry.getKey();
+                double sum = entry.getValue() + normalizedDailyValue.get(key) + normalizedWeeklyValue.get(key) + normalizedMonthlyValue.get(key);
+                result.put(entry.getKey() , sum);
+            }
+
+            return result;
+
+        } catch (Exception e) {
+            String errorLog = "Error : while converting data in videoSuggest !";
+            System.err.println(errorLog);
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private HashMap<String , Double> dataConversion(HashMap<String, Long>  data, double percentage) throws Exception {
+        HashMap<String , Double> result = new HashMap<>();
+        for (Map.Entry<String , Long> set : data.entrySet()) {
+            result.put(set.getKey(), ((set.getValue() / (double) data.get("sum")) * percentage));
 
         }
         return result;
