6bd5813d0e5dc7ff0cce4afc87c72e55c2af7a00
woxane <mohsen.minavand1384@gmail.com>
2024-07-08 10:03:15 +0330

    feat(Recommendation): implement constructor and dataConversion function

--- src/main/java/Server/Recommendation.java
+++ src/main/java/Server/Recommendation.java
@@ -1,4 +1,23 @@
 package Server;
 
+import Shared.Models.Category;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Recommendation {
+    private Long channelId;
+
+    public Recommendation(Long channelId) {
+        this.channelId = channelId;
+    }
+
+    public HashMap<Category, Double> dataConversion(HashMap<Category, Integer>  data, double percentage) throws Exception {
+        HashMap<Category , Double> result = new HashMap<>();
+        for (Map.Entry<Category , Integer> set : data.entrySet()) {
+            result.put(set.getKey(), (set.getValue() / (double) data.get("sum")) * percentage);
+
+        }
+        return result;
+    }
 }
