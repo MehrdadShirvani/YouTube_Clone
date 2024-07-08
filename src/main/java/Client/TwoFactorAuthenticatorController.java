0f5981b8b1d4523999b8148101d716c55cc167a7
woxane <mohsen.minavand1384@gmail.com>
2024-07-08 16:59:00 +0330

    feat(TwoFactorAuthenticator): implement initialize function with login button and useEmailButton

--- src/main/java/Client/TwoFactorAuthenticatorController.java
+++ src/main/java/Client/TwoFactorAuthenticatorController.java
@@ -1,16 +1,36 @@
 package Client;
 
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextArea;
+
 import java.awt.event.ActionEvent;
 
 public class TwoFactorAuthenticatorController {
-    public void useEmailButton(ActionEvent actionEvent) {
-        //TODO change scene to useEmailButton
+    @FXML
+    private TextArea codeField;
+    private int codeDigit;
 
+
+    public void initialize() {
+        this.codeDigit = YouTube.client.twoFactorEmailSend();
+
+    }
+    public void useEmailButton(ActionEvent actionEvent) {
+        YouTube.changeScene("two-factor-email-view.fxml");
     }
 
 
     public void loginButton(ActionEvent actionEvent) {
-        //TODO login
+        String codeString = codeField.getText();
+        int code = Integer.getInteger(codeString);
 
+        if (code == codeDigit) {
+            YouTube.changeScene("home-view.fxml");
+
+        } else {
+            //TODO : pop up error
+        }
     }
 }
