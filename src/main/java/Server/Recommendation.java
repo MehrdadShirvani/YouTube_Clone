3926253dbf634e347062811937cf10424a605fdc
woxane <mohsen.minavand1384@gmail.com>
2024-07-08 10:06:17 +0330

    feat(Recommendation): implement calculateStartDate function

--- src/main/java/Server/Recommendation.java
+++ src/main/java/Server/Recommendation.java
@@ -4,6 +4,8 @@ import Shared.Models.Category;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Date;
+import java.util.Calendar;
 
 public class Recommendation {
     private Long channelId;
@@ -12,12 +14,19 @@ public class Recommendation {
         this.channelId = channelId;
     }
 
-    public HashMap<Category, Double> dataConversion(HashMap<Category, Integer>  data, double percentage) throws Exception {
+    private HashMap<Category, Double> dataConversion(HashMap<Category, Integer>  data, double percentage) throws Exception {
         HashMap<Category , Double> result = new HashMap<>();
         for (Map.Entry<Category , Integer> set : data.entrySet()) {
             result.put(set.getKey(), (set.getValue() / (double) data.get("sum")) * percentage);
 
         }
         return result;
+    }
+
+    private Date calculateStartDate(Date endDate, int field, int amount) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(endDate);
+        calendar.add(field, amount);
+        return calendar.getTime();
     }
 }
