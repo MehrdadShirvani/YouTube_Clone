cf8c86caa928e6dfc4c92022304769c70f909fb2
Ξhsan (3H) <43131138+Ehsan-Habib-3H@users.noreply.github.com>
2024-07-10 00:02:16 +0330

    feat: handle user input in add playlist

--- src/main/java/Client/AddEditPlaylistController.java
+++ src/main/java/Client/AddEditPlaylistController.java
@@ -2,24 +2,24 @@ package Client;
 
 import Shared.Models.Playlist;
 import javafx.event.ActionEvent;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.VBox;
 
 import java.util.Objects;
 
 public class AddEditPlaylistController {
     public TextField TxtName;
+    public VBox txtVBox;
     public ToggleButton CBIsPrivate;
     public ToggleButton CBIsCollaborative;
     public Button SubmitBtn;
     private Playlist playlist;
+    private Label errLabel;
 
-    public void setPlaylist(Playlist playlist, HomeController controller)
-    {
+    public void setPlaylist(Playlist playlist, HomeController controller) {
         this.playlist = playlist;
-        if(playlist != null) {
+        if (playlist != null) {
             TxtName.setText(playlist.getName());
             //Normal 1
             //Collaborative 2
@@ -29,29 +29,23 @@ public class AddEditPlaylistController {
             CBIsCollaborative.setDisable(playlist.getPrivate());
         }
     }
-    public void submitAction(ActionEvent actionEvent)
-    {
-        if(TxtName.getText().isBlank())
-        {
-            //TODO Ehsan show error
+
+    public void submitAction(ActionEvent actionEvent) {
+        if (TxtName.getText().isBlank()) {
             return;
         }
-        if(Objects.equals(TxtName.getText(), "Watch Later"))
-        {
+        if (Objects.equals(TxtName.getText(), "Watch Later")) {
             //TODO Ehsan show error
             return;
         }
 
-        if(playlist == null)
-        {
-           Playlist newPlaylist =  YouTube.client.addPlaylist(new Playlist(TxtName.getText(),CBIsCollaborative.isSelected()?(short)2:(short)1, CBIsPrivate.isSelected()));
-           YouTube.client.addChannelPlaylist(YouTube.client.getAccount().getChannelId(),newPlaylist.getPlaylistId());
+        if (playlist == null) {
+            Playlist newPlaylist = YouTube.client.addPlaylist(new Playlist(TxtName.getText(), CBIsCollaborative.isSelected() ? (short) 2 : (short) 1, CBIsPrivate.isSelected()));
+            YouTube.client.addChannelPlaylist(YouTube.client.getAccount().getChannelId(), newPlaylist.getPlaylistId());
             //TODO Ehsan -> get back to page
-        }
-        else
-        {
+        } else {
             playlist.setName(TxtName.getText());
-            playlist.setPlaylistTypeId(CBIsCollaborative.isSelected()?(short)2:(short)1);
+            playlist.setPlaylistTypeId(CBIsCollaborative.isSelected() ? (short) 2 : (short) 1);
             playlist.setPrivate(CBIsPrivate.isSelected());
             playlist = YouTube.client.editPlaylist(playlist);
             //TODO Ehsan -> get back to page
@@ -59,12 +53,35 @@ public class AddEditPlaylistController {
 
     }
 
-    public void cbPrivateAction(ActionEvent actionEvent)
-    {
-        CBIsCollaborative.setDisable(CBIsPrivate.isSelected());
-        if(CBIsCollaborative.isDisable())
-        {
-            CBIsCollaborative.setSelected(false);
+    public void checkValidation(KeyEvent keyEvent) {
+        if (errLabel == null) {
+            errLabel = new Label();
+            errLabel.getStyleClass().add("err-label");
+        }
+        errLabel.setText("");
+        if (TxtName.getText().isBlank()) {
+            errLabel.setText("*Shouldn't be blank!");
+            SubmitBtn.setDisable(true);
+            try {
+                txtVBox.getChildren().add(errLabel);
+            }
+            catch (Exception e)
+            {}
+        } else if (Objects.equals(TxtName.getText(), "Watch Later")) {
+            SubmitBtn.setDisable(true);
+            errLabel.setText("*Reserved name!");
+            try {
+                txtVBox.getChildren().add(errLabel);
+            }
+            catch (Exception e)
+            {}
+        } else {
+            SubmitBtn.setDisable(false);
+            try {
+                txtVBox.getChildren().remove(errLabel);
+            }
+            catch (Exception e)
+            {}
         }
     }
 }
