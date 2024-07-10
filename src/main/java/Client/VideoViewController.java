b87b92b681fc752b090a7233ee6174258fac1c6c
Mehrdad Shirvani <mehrdadsh0901@gmail.com>
2024-07-10 13:45:02 +0330

    feat(VideoViewController): added download functionality

--- src/main/java/Client/VideoViewController.java
+++ src/main/java/Client/VideoViewController.java
@@ -26,15 +26,21 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.awt.*;
 
 import java.awt.datatransfer.StringSelection;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
+import java.io.InputStream;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.DecimalFormat;
@@ -45,8 +51,6 @@ import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 public class VideoViewController {
     public Label titleLabel;
@@ -282,6 +286,7 @@ public class VideoViewController {
                             sideBarVBox.getChildren().add(smallVideo);
                         }
                     }
+
                     isChannelSubscribed = YouTube.client.isSubscribedToChannel(video.getChannelId());
                     if (isChannelSubscribed) {
                         subsIcon.setContent("M10 20H14C14 21.1 13.1 22 12 22C10.9 22 10 21.1 10 20ZM20 17.35V19H4V17.35L6 15.47V10.32C6 7.40001 7.56 5.10001 10 4.34001V3.96001C10 2.54001 11.49 1.46001 12.99 2.20001C13.64 2.52001 14 3.23001 14 3.96001V4.35001C16.44 5.10001 18 7.41001 18 10.33V15.48L20 17.35ZM19 17.77L17 15.89V10.42C17 7.95001 15.81 6.06001 13.87 5.32001C12.61 4.79001 11.23 4.82001 10.03 5.35001C8.15 6.11001 7 7.99001 7 10.42V15.89L5 17.77V18H19V17.77Z");
@@ -525,8 +530,60 @@ public class VideoViewController {
     }
 
     public void downloadAction(ActionEvent event) {
+        if(YouTube.client.getAccount().getPremiumExpirationDate() == null || YouTube.client.getAccount().getPremiumExpirationDate().before(new Date()))
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("You're not a premium user!");
+            alert.showAndWait();
+            //TODO better alert
+            return;
+        }
+
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Video File");
+        fileChooser.setInitialFileName(video.getName() + ".mp4");
+        FileChooser.ExtensionFilter filter = new FileChooser.ExtensionFilter("MP4 files (*.mp4)", "*.mp4");
+        fileChooser.getExtensionFilters().add(filter);
+
+        Stage dialogStage = new Stage();
+        File selectedFile = fileChooser.showSaveDialog(dialogStage);
+
+        if (selectedFile != null) {
+            downloadVideo(selectedFile);
+        }
+    }
+    private void downloadVideo(File selectedFile)
+    {
+        executorService.submit(() -> {
+
+            try {
+                HttpClient httpClient = HttpClient.newHttpClient();
+                HttpRequest request = HttpRequest.newBuilder()
+                        .uri(new URI("http://localhost:2131/download/" + video.getVideoId()))
+                        .build();
+                HttpResponse<InputStream> response = httpClient.send(request, HttpResponse.BodyHandlers.ofInputStream());
+                if (response.statusCode() == 200) {
+                    try (InputStream is = response.body(); FileOutputStream fos = new FileOutputStream(selectedFile)) {
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = 0;
+                        while ((bytesRead = is.read(buffer)) != -1) {
+                            fos.write(buffer, 0, bytesRead);
+                        }
+                    }
+                } else {
+                    //TODO -> show some error
+                }
+            } catch (URISyntaxException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        });
 
     }
+
 
     public void editBtnAction(ActionEvent actionEvent) {
         homeController.setVideoEditingPage(video);
